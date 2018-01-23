6a827ff10015c322e8180c4aa733c278d1ffbf9e
Daniel Felipe Mejía Sañudo - Ceiba Software <daniel.mejia@ceiba.com.co>
2018-01-23 10:39:47 -0500

    junit

--- restparqueadero/src/test/java/com/co/ceiba/restparqueadero/RestparqueaderoApplicationTests.java
+++ restparqueadero/src/test/java/com/co/ceiba/restparqueadero/RestparqueaderoApplicationTests.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
